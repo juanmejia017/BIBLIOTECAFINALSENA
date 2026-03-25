6fa83c76c628771c7e65c365cbda7bb1b5920eb8
juanmejia017 <juanmejia0712123@gmail.com>
2026-03-25 12:03:18 -0500

    inital menu

--- FINALBIBLIOTECAC/Program.cs
+++ FINALBIBLIOTECAC/Program.cs
@@ -18,7 +18,40 @@ namespace ProyectoBibliotecaSENA
 
             ShowMainMenu();
             }
+            // ==========================================
+        // 1. ESTRUCTURA DEL MENÚ PRINCIPAL
+        // ==========================================
+        static void ShowMainMenu()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("=== MENÚ PRINCIPAL ===");
+                Console.WriteLine("1. Libros");
+                Console.WriteLine("2. Usuarios");
+                Console.WriteLine("3. Préstamos");
+                Console.WriteLine("4. Búsquedas y reportes");
+                Console.WriteLine("5. Guardar / Cargar datos");
+                Console.WriteLine("6. Salir");
+                Console.Write("\nSeleccione una opción: ");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1": ShowBooksMenu(); break;
+                    case "2": ShowUsersMenu(); break;
+                    case "3": ShowLoansMenu(); break;
+                    case "4": ShowSearchReportsMenu(); break;
+                    case "5": ShowPersistenceMenu(); break;
+                    case "6": exit = ConfirmExitAndSave(); break;
+                    default: ShowInvalidMessage(); break;
+                }
             }
-            
-            
-            }
+        }
+         }
+         
+         
+         
+         }
